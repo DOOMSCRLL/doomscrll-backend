4a99084baa595fcb45276e155b3e99315f6882f5
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-06-25 23:35:12 +0300

    chore: remove debugging scripts

--- test-db.cjs
+++
@@ -1,6 +0,0 @@
-const postgres = require("postgres")
-const sql = postgres("postgres://postgres:postgres@localhost:5432/doomscrll")
-sql`SELECT category, status, COUNT(*) FROM projects GROUP BY category, status`.then((res) => {
-	console.log(res)
-	process.exit(0)
-})
--- test-db2.cjs
+++
@@ -1,8 +0,0 @@
-const postgres = require("postgres")
-const sql = postgres("postgres://postgres:doomscrllrocks@127.0.0.1:5432/postgres")
-sql`SELECT category, status, COUNT(*) FROM projects WHERE showcase_date >= '2026-06-25' GROUP BY category, status`
-	.then((res) => {
-		console.log(res)
-		process.exit(0)
-	})
-	.catch(console.error)
